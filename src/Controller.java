da9c2a2256a403f37ffee004bd5a961267575fca
EliasBerhe <eliastes123@gmail.com>
2023-07-06 19:29:47 -0400

    finished setting up player.fxml

---
+++ src/Controller.java
@@ -0,0 +1,23 @@
+public class Controller {
+
+
+    public void playMedia(){
+
+    }
+    public void pauseMedia(){
+
+    }
+    public void previousMedia(){
+
+    }
+    public void nextMedia(){
+
+    }
+    public void resetMedia(){
+
+    }
+
+    public void changeSpeed(){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,7 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("player.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 }
