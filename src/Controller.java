d41fa2a0aa807e100d0c887d1dc434caea35fbfe
EliasBerhe <eliastes123@gmail.com>
2023-07-06 19:40:56 -0400

    finished initalize method

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,9 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -22,7 +22,8 @@ public class Controller implements Initializable {
 
     @FXML
     private Label label;
-
+    @FXML
+    private Slider volumeSlider;
     @FXML
     private Button playButton;
 
@@ -62,6 +63,35 @@ public class Controller implements Initializable {
     private Media media;
 
     private MediaPlayer mediaPlayer;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File song1 = new File("src/music/song1.mp3");
+        File song2 = new File("src/music/song2.mp3");
+        File song3 = new File("src/music/song3.mp3");
+        songs = new ArrayList<File>();
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3);
+
+        media  = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        label.setText(songs.get(songNumber).getName());
+
+        for(int i=0; i<speeds.length;i++){
+            speedBox.getItems().add(Integer.toString(speeds[i])+ "%");
+        }
+        speedBox.setOnAction(this::changeSpeed); // call changeSpeed
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                mediaPlayer.setVolume(volumeSlider.getValue()*0.01);
+            }
+        });
+        progressBar.setStyle("-fx-accent: #00FF00");
+    }
     public void playMedia(){
 
     }
@@ -78,12 +108,9 @@ public class Controller implements Initializable {
 
     }
 
-    public void changeSpeed(){
+    public void changeSpeed(ActionEvent event){
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
 }
