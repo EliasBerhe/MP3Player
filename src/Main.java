a52ba3f62b5851e857641069811dddfd987b3af5
EliasBerhe <eliastes123@gmail.com>
2023-07-06 20:01:02 -0400

    some final touches

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -17,7 +18,10 @@ public class Main extends Application {
         try{
             Parent root = FXMLLoader.load(getClass().getResource("player.fxml"));
             Scene scene = new Scene(root);
+            Image icon = new Image("icon2.jpg");
+            stage.getIcons().add(icon);
             stage.setScene(scene);
+            stage.setResizable(false);
             stage.show();
 
             stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
